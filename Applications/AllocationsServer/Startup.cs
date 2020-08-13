39e1589a464bab8b3dfb04ec78a6a6ca56b66942
DasuSrikanth <dasusrikanth@gmail.com>
2020-08-13 19:33:11 +0530

    security

--- Applications/AllocationsServer/Startup.cs
+++ Applications/AllocationsServer/Startup.cs
@@ -13,6 +13,12 @@ using Steeltoe.Discovery.Client;
 using Steeltoe.Common.Discovery;
  using Microsoft.Extensions.Logging;
  using Steeltoe.CircuitBreaker.Hystrix;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Steeltoe.Security.Authentication.CloudFoundry;
+ using Microsoft.AspNetCore.Authentication;
+ using Microsoft.AspNetCore.Http;
 
 namespace AllocationsServer
 {
@@ -29,13 +35,27 @@ namespace AllocationsServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCloudFoundryActuators(Configuration);
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddCloudFoundryJwtBearer(Configuration);
+            //services.AddControllers();
+                       services.AddControllers(mvcOptions =>
+            {
+                if (!Configuration.GetValue("DISABLE_AUTH", false))
+                {
+                    // Set Authorized as default policy
+                    var policy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme)
+                        .RequireAuthenticatedUser()
+                        .RequireClaim("scope", "uaa.resource")
+                        .Build();
 
-            services.AddControllers();
+                    mvcOptions.Filters.Add(new AuthorizeFilter(policy));
+                }
+            });
             services.AddDiscoveryClient(Configuration);
 
             services.AddScoped<IAllocationDataGateway, AllocationDataGateway>();
             services.AddDbContext<AllocationContext>(options => options.UseMySql(Configuration));
-            
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddSingleton<IProjectClient>(sp =>
             {
 
@@ -45,7 +65,14 @@ namespace AllocationsServer
                     BaseAddress = new Uri(Configuration.GetValue<string>("REGISTRATION_SERVER_ENDPOINT"))
                 };
                  var logger = sp.GetService<ILogger<ProjectClient>>();
-                 return new ProjectClient(httpClient, logger);
+                // return new ProjectClient(httpClient, logger);
+                
+                var contextAccessor = sp.GetService<IHttpContextAccessor>();
+
+                return new ProjectClient(
+                    httpClient, logger,
+                    () => contextAccessor.HttpContext.GetTokenAsync("access_token")
+                );
               });
 
              services.AddHystrixMetricsStream(Configuration);
--- Applications/BacklogServer/Startup.cs
+++ Applications/BacklogServer/Startup.cs
@@ -13,6 +13,13 @@ using Steeltoe.Discovery.Client;
 using Steeltoe.Common.Discovery;
  using Microsoft.Extensions.Logging;
  using Steeltoe.CircuitBreaker.Hystrix;
+ using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Steeltoe.Security.Authentication.CloudFoundry;
+ using Microsoft.AspNetCore.Authentication;
+ using Microsoft.AspNetCore.Http;
+
 
 namespace BacklogServer
 {
@@ -29,12 +36,26 @@ namespace BacklogServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCloudFoundryActuators(Configuration);
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddCloudFoundryJwtBearer(Configuration);
+           // services.AddControllers();
+                      services.AddControllers(mvcOptions =>
+            {
+                if (!Configuration.GetValue("DISABLE_AUTH", false))
+                {
+                    // Set Authorized as default policy
+                    var policy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme)
+                        .RequireAuthenticatedUser()
+                        .RequireClaim("scope", "uaa.resource")
+                        .Build();
 
-            services.AddControllers();
+                    mvcOptions.Filters.Add(new AuthorizeFilter(policy));
+                }
+            });
             services.AddDiscoveryClient(Configuration);
             services.AddDbContext<StoryContext>(options => options.UseMySql(Configuration));
             services.AddScoped<IStoryDataGateway, StoryDataGateway>();
-
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddSingleton<IProjectClient>(sp =>
             {
                 var handler = new DiscoveryHttpClientHandler(sp.GetService<IDiscoveryClient>());
@@ -43,7 +64,14 @@ namespace BacklogServer
                     BaseAddress = new Uri(Configuration.GetValue<string>("REGISTRATION_SERVER_ENDPOINT"))
                 };
                  var logger = sp.GetService<ILogger<ProjectClient>>();
-                 return new ProjectClient(httpClient, logger);
+                // return new ProjectClient(httpClient, logger);
+                
+                var contextAccessor = sp.GetService<IHttpContextAccessor>();
+
+                return new ProjectClient(
+                    httpClient, logger,
+                    () => contextAccessor.HttpContext.GetTokenAsync("access_token")
+                );
               });
 
              services.AddHystrixMetricsStream(Configuration);
